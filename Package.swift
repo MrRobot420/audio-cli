95cce4e8c02498f5f619db253797cdfa3e09f2e6
MrRobot420 <max.karlchen@web.de>
2022-05-07 11:39:39 +0200

    finally get the Package.swift file right

--- Package.swift
+++ Package.swift
@@ -1,20 +1,26 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5.0
 
 import PackageDescription
 
 let package = Package(
     name: "audio-cli",
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .executable(name: "audio-cli", targets: ["audio-cli"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "0.5.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "audio-cli",
-            dependencies: []),
+            dependencies: ["ColorizeSwift", .product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "audio-cliTests",
             dependencies: ["audio-cli"]),
