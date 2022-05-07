cdc6a70d8d7ca1fb60a8674fca8fa9c1c642edb1
MrRobot420 <max.karlchen@web.de>
2022-05-07 11:50:38 +0200

    get first cli draft to work

--- Sources/audio-cli/main.swift
+++ Sources/audio-cli/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct AudioCLI: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "A Swift command-line tool to stream audio files to multiple outputs.",
+        version: "0.1.0",
+        subcommands: [Generate.self])
+
+    init() { }
+}
+
+struct Generate: ParsableCommand {
+    public static let configuration = CommandConfiguration(abstract: "Generate multiple audio outputs from the given audio file-path.")
+
+    @Argument(help: "The audio file path.")
+    private var filePath: String
+
+    func run() throws {
+        print("Creating outputs for file \"\(filePath)\"")
+    }
+}
+
+AudioCLI.main()
